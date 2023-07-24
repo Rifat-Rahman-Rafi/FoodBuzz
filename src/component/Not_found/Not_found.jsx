749986eb212c2babd052048478748e2106aff09a
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-07-24 21:37:00 +0600

    Not_foun_page

--- src/component/Not_found/Not_found.jsx
+++ src/component/Not_found/Not_found.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
 
 const Not_found = () => {
-    // const { error, status } = useRouteError()
+    const { error, status } = useRouteError()
     return (
-        // <div className='text-center'>
-        //     <img style={{height:400,width:800}} className='mx-auto' src="https://i.ibb.co/ZHPJ7vY/9097-png-860.png" alt="" />
-        //     <div>
-        //     <h3 className='mb-8 font-extrabold text-9xl text-gray-600'>
-        //     <span className='sr-only'>Error</span> {status || 404}
-        //   </h3>
+        <div className='text-center'>
+            <img style={{height:400,width:800}} className='mx-auto' src="https://i.ibb.co/ZHPJ7vY/9097-png-860.png" alt="" />
+            <div>
+            <h3 className='mb-8 font-extrabold text-9xl text-gray-600'>
+            <span className='sr-only'>Error</span> {status || 404}
+          </h3>
      
-        //        <Link to='/'><button className='bg-[#FFB01F] font-bold text-white p-3 rounded-md'>GOT TO HOME PAGE</button></Link>
-        //     </div>
-        // </div>
+               <Link to='/'><button className='bg-[#FFB01F] font-bold text-white p-3 rounded-md'>GOT TO HOME PAGE</button></Link>
+            </div>
+        </div>
         
         )
 };
